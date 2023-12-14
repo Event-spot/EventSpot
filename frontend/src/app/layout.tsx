b3b84989520f1b0e8c5866826b725ed5f5c987b1
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2023-12-14 07:38:21 +0100

    Change globals.css to globals.scss.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './globals.scss'
 import Navbar from "@/layout/Navbar/Navbar";
 import Footer from "@/layout/Footer/Footer";
 
