d3cdaeb44d24e129492c06332421c55ef29cd222
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2024-01-20 23:10:58 +0100

    Change entities names and add migrations

--- api/src/config/typeorm.config.ts
+++ api/src/config/typeorm.config.ts
@@ -12,7 +12,7 @@ export default class TypeOrmConfig {
             password: configService.get("DB_PASSWORD"),
             database: configService.get("DB_NAME"),
             entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-            synchronize: true
+            synchronize: false
         }
     }
 }
