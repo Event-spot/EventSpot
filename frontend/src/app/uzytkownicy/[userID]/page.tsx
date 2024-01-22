b9ddf8b5ebcd2b6ea9dfab7e5abc5b44ee03f658
DAWID JASIECZKO <dawidjasieczko@gmail.com>
2024-01-22 20:10:16 +0100

    Revert "Profile Timeline"
    This reverts commit eb66d336ebc8dd587f373f89ad86ccb5b4cb268f.

--- frontend/src/app/uzytkownicy/[userID]/page.tsx
+++ frontend/src/app/uzytkownicy/[userID]/page.tsx
@@ -5,13 +5,6 @@ import styles from './profile.module.scss';
 import Image from "next/image";
 import Profile_Background from '../../../assets/images/profile_background.png';
 import Question from '../../../assets/images/question.png'
-import { RiFacebookBoxFill } from "react-icons/ri";
-import { RiInstagramFill } from "react-icons/ri";
-import { RiTiktokFill } from "react-icons/ri";
-import { RiYoutubeFill } from "react-icons/ri";
-import Followers from "../../../components/Followers/Followers";
-import EventHistory from "../../../components/EventHistory/EventHistory";
-
 
 
 type User = {
@@ -32,9 +25,10 @@ export default function Profile({ params: { userID } }: Params) {
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
-  const user = data.users.find((user: User) => user.id === parseInt(userID, 10));
-  if (!user) return <p>User not found</p>;
 
+  const user = data.users.find((user: User) => user.id === parseInt(userID, 10));
+
+  if (!user) return <p>User not found</p>;
 
   return (
     <div className={styles.main}>
@@ -48,7 +42,6 @@ export default function Profile({ params: { userID } }: Params) {
         />
         <div className={styles.overlay}></div>
       </div>
-
       <div className={styles.profileDiv}>
         <div className={styles.profileLeft}>
           <div className={styles.profileSquare}>
@@ -62,30 +55,92 @@ export default function Profile({ params: { userID } }: Params) {
             <p>Obserwujących: {user.followers}</p>
           </div>
         </div>
-        <div className={styles.divButton}>
+        <div>
           <button className={styles.button}>+ Obserwuj</button>
         </div>
       </div>
       
+
       <div className={styles.next}>
-        <div className={styles.profileInfo}>
-          <fieldset className={styles.description}>
-            <legend>O mnie </legend>
-          </fieldset>
-          <div className={styles.contact}>
-            <RiFacebookBoxFill color="#4968ad" />
-            <RiInstagramFill color="#e1306c" />
-            <RiTiktokFill color="rgb(30, 48, 80)" />
-            <RiYoutubeFill color="#eb3223" />
-          </div>
+        <div className={styles.insidenext1}>
+        <h1>inside1</h1>
         </div>
-          <div className={styles.insidenext2}>
-          <EventHistory />
-          </div>
-          <div className={styles.followersContainer}>
-            <Followers userId={userID}/>
-          </div>
+        <div className={styles.insidenext2}>
+         <h2>inside2</h2>
+        </div>
+        <div className={styles.insidenext3}></div>
+      </div>
+
+      <div className={styles.afternext}>
+        <div className={styles.komentarze}></div>
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+{/* <div className={styles["event-container"]}>
+      <Image
+        loading="lazy"
+        src= { Profile_Background} className={styles["event-image"]}
+        alt="Event Image"
+        width={1000}
+        height={300}
+      />
+      <div className={styles["event-info"]}>
+        <div className={styles["info-column"]}>
+          <span className={styles["info-label"]}>Informacje</span>
+          <div className={styles["info-item"]}>
+            <span className={styles["label"]}>Lokalizacja:</span>
+            <span className={styles["value"]}>Bytom</span>
+          </div>
+          <div className={styles["info-item"]}>
+            <span className={styles["label"]}>Zorganizowane wydarzenia:</span>
+            <span className={styles["value"]}>32</span>
+          </div>
+        </div>
+      </div>
+    </div> */}
+
+
+
+
+
+
+
+
+
+//     <div className={styles.main}>
+//       <div className={styles.header}>
+//       <Image priority={true} className={styles.header_img} src={Profile_Background} alt={'Profile Background'}/>  
+//       {/* <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/90823df5189b5075c476622f72bbfcea8c0a9efbc2be8bba5eddf8f9f4e90a4f?apiKey=6af31a49a00741fea57d0c082109faff&" class="header-img" /> */}
+//       </div>
+//     {/* <div className={styles.img}>
+//     <Image priority={true} className={styles.avatar} src={Profile_Background} alt={'Profile Background'}/>  
+//     </div> */}
+
+//     <div className={styles.user}>
+//     <div className={styles.pages}>
+//         <div className={styles.container}>
+//             <div>
+//               <h1>User Details</h1>
+//               <p>First Name: {user.firstname}</p>
+//               <p>Last Name: {user.lastname}</p>
+//             </div>
+//         </div>
+//       </div>
+//     </div>
+// </div>
+  //);
+//}
